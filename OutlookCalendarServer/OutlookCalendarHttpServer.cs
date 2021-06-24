28e0718fbbb9e8dd1ca34b67e8b37b1e8d30f6b5
koderush <koderush@outlook.com>
2021-06-24 10:53:38 -0600

    Add files via upload

---
+++ OutlookCalendarServer/OutlookCalendarHttpServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Outlook;
+using Bend.Util;
+
+namespace OutlookCalendarServer
+{
+    public class OutlookCalendarHttpServer : HttpServer
+    {
+        private static DateTime EPOCH = new DateTime(1970, 1, 1);
+
+        public OutlookCalendarHttpServer(int port)
+            : base(port)
+        {
+        }
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            if (p.http_url.Equals("/outlookcalendar"))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                List<AppointmentItem> OutlookEntries = OutlookCalendar.Instance.getCalendarEntriesInRange();
+                foreach (AppointmentItem ai in OutlookEntries)
+                {
+                    sb.Append(ai.Start + "|" + getTimestamp(ai.Start.ToUniversalTime()) + "|" + ai.End + "|" + getTimestamp(ai.End.ToUniversalTime()) + "|" + ai.Subject + "|" + ai.Location + "|\n");
+                }
+
+                p.outputStream.BaseStream.Write(Encoding.ASCII.GetBytes(sb.ToString()),0,sb.Length);
+                p.outputStream.BaseStream.Flush();
+            }
+        }
+
+        private long getTimestamp(DateTime dateTime)
+        {
+            TimeSpan ts = dateTime - EPOCH;
+
+            return (long) ts.TotalSeconds;
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+        }
+    }
+}
---
+++ OutlookCalendarServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Bend.Util;
+
+namespace OutlookCalendarServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer httpServer;
+            if (args.GetLength(0) > 0)
+            {
+                httpServer = new OutlookCalendarHttpServer(Convert.ToInt16(args[0]));
+            }
+            else
+            {
+                httpServer = new OutlookCalendarHttpServer(8777);
+            }
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+        }
+    }
+}
